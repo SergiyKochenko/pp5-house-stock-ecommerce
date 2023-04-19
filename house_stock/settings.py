92794f2d3f261e2f0ebdfe16bf98e7e00ad3ab25
SergiyKochenko <sergiykochenko@gmail.com>
2023-04-19 10:02:53 +0000

    removed secret key and set bebug

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8zh#dwkt(i-*m-vbp0l8f+ux!tgvd-=+$3-9#_dqg&i!4c&%mx'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pp5-house-stock-ecommerce.herokuapp.com', 'localhost']
 
