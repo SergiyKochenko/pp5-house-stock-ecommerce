587fb5713a523ec9d11a35a64f3e67b31f3c28df
SergiyKochenko <sergiykochenko@gmail.com>
2023-04-28 00:47:05 +0000

    fixing static deployment bug

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['pp5-house-stock-ecommerce.herokuapp.com', 'localhost']
 
