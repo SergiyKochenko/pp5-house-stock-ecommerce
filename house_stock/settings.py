88486078de6f114cd7709d0f040c56bf5219cae2
SergiyKochenko <>
2023-04-30 12:07:06 +0000

    successfully setup redository on codeanywhere

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['pp5-house-stock-ecommerce.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp5-house-stock-ecommerce.herokuapp.com', 'localhost', '*']
 
 
 # Application definition
