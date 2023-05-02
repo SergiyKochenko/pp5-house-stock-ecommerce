26aeb39dacd2fef0f5cae30dd962a05530b4abf9
SergiyKochenko <>
2023-05-02 10:07:17 +0000

    fixing socialaccount bug

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -49,9 +49,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    #  ============
     "allauth.socialaccount.providers.github",
-    # =============
     'home',
     'products',
     'bag',
