0e9563f36c2b0d81b52f66dd0756a225abf6e919
SergiyKochenko <>
2023-05-03 23:05:35 +0000

    fix bug 2

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -157,12 +157,12 @@ else:
         }
     }
 
-# # DATABASES = {
-# #      'default': {
-# #          'ENGINE': 'django.db.backends.sqlite3',
-# #          'NAME': BASE_DIR / 'db.sqlite3',
-# #      }
-# # }
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# }
 
 
 # Password validation
