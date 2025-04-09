c6809f868a5de64ae08f8a56bbbbfce50391f87a
Sergiy <sergiykochenko@gmail.com>
2025-04-09 21:29:35 +0100

    .

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -278,3 +278,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
