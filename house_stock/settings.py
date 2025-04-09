17003455c3440ba4bb447d4d0de8396ad3b6ad6a
Sergiy <sergiykochenko@gmail.com>
2025-04-09 23:06:19 +0100

    .

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -159,12 +159,12 @@ DATABASES = {
 
 # =========================================================
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
-}
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# }
 
 # ========================================================
 
