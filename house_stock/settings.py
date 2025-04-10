b02623c50e3fb00e2b9ce3f56494190739ae19e6
Sergiy <sergiykochenko@gmail.com>
2025-04-10 01:01:58 +0100

    .

--- house_stock/settings.py
+++ house_stock/settings.py
@@ -222,31 +222,6 @@ cloudinary.config(
 )
 
 
-# if 'USE_AWS' in os.environ:
-
-#     # Cache control
-#     AWS_S3_OBJECT_PARAMETERS = {
-#         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-#         'CacheControl': 'max-age=94608000',
-#     }
-
-#     # Bucket Config
-#     AWS_STORAGE_BUCKET_NAME = 'pp5-house-stock-ecommerce'
-#     AWS_S3_REGION_NAME = 'us-east-1'
-#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-#     # Static and media files
-#     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-#     STATICFILES_LOCATION = 'static'
-#     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-#     MEDIAFILES_LOCATION = 'media'
-
-#     # Override static and media URLs in production
-#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-#     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 if 'USE_AWS' in os.environ:
     # -----------------------------------------------------------------------------
     # AWS S3 Configuration for Static and Media Files
@@ -308,7 +283,8 @@ if 'USE_AWS' in os.environ:
     # -----------------------------------------------------------------------------
     check_bucket_status(AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
 else:
-    print("AWS is not enabled; 'USE_AWS' environment variable not set.")
+    # Use Cloudinary for media storage
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # delivery threshold
 FREE_DELIVERY_THRESHOLD = 99
